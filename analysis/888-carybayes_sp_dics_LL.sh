f7287f08bf2c04bea209dec7a8164afebb3c79a6
Nicholas Brazeau <nbrazeau@med.unc.edu>
2019-12-11 14:00:02 +0000

    script for LL

---
+++ analysis/888-carybayes_sp_dics_LL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --time=36:00:00
+#SBATCH --mem=49512
+#SBATCH --mail-type=all
+#SBATCH --mail-user=nbrazeau@med.unc.edu
+
+Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/Space_the_Final_Miptier"); source("analyses/04-BayesSPlambda_backend.R")'
