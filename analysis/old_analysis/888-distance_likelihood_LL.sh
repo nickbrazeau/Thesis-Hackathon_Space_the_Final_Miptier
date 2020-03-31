59f941c33de05a2d262cdbf3c25e000387016680
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-03-31 10:40:46 -0400

    post thesis purge

--- analysis/old_analysis/888-distance_likelihood_LL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --ntasks=1
-#SBATCH --time=5-00:00:00
-#SBATCH --mem=256g
-#SBATCH --mail-type=all
-#SBATCH --mail-user=nbrazeau@med.unc.edu
-
-Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/Space_the_Final_Miptier"); source("analysis/03-IBD_S_likelihood_backend.R")'
--- analysis/old_analysis/888-meiotic_siblings_LL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --ntasks=1
-#SBATCH --time=5-00:00:00
-#SBATCH --mem=128g
-#SBATCH --mail-type=all
-#SBATCH --mail-user=nbrazeau@med.unc.edu
-
-Rscript -e 'setwd("/proj/ideel/meshnick/users/NickB/Projects/Space_the_Final_Miptier"); source("analysis/05-IBD_meioticsibs_backend.R")'
