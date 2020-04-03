008148cb5c140af615d06b65dc9b752ed6fd40a4
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-04-02 20:36:48 -0400

    parallelize LL

--- analysis/cluster_inbreeding/clstinb_launch.sh
+++ analysis/cluster_inbreeding/clstinb_launch.sh
@@ -2,12 +2,15 @@
 
 ROOT=/proj/ideel/meshnick/users/NickB/Projects/Space_the_Final_Miptier/ # root directory for project (non-scratch)
 SNAKE=/proj/ideel/meshnick/users/NickB/Projects/Space_the_Final_Miptier/analysis/cluster_inbreeding
+NODES=1028 # max number of cluster nodes
 WAIT=30 # lag for system
 
 snakemake \
 	--snakefile $SNAKE/run_snake_params.py \
 	--configfile $SNAKE/config_temp.yaml \
 	--directory $ROOT \
+	--cluster $ROOT/launch.py \
+	-j $NODES \
 	--printshellcmds \
 	--rerun-incomplete \
 	--keep-going \
