84be9c8e5f5c384e1e667855219c213f3a6e0951
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-05-13 10:13:55 -0400

    update calls to gd

--- analysis/cluster_inbreeding/01-collect_results.R
+++ analysis/cluster_inbreeding/01-collect_results.R
@@ -48,21 +48,46 @@ clst_ret.list <- lapply(filepaths, read_results, clstnames = sort(clsts$hv001))
 clst_ret <- clst_ret.list %>%
   dplyr::bind_rows()
 
-mastermap.results <- dplyr::left_join(mastermap.sm, clst_ret, by = "param_set")
+mastermap.results <- dplyr::left_join(mastermap.sm, clst_ret, by = "param_set") %>%
+  dplyr::select(c("fstart", "mstart", "learningrate", "param_set", "distance", "m",
+                  dplyr::everything()))
 
 #..............................................................
 # explore results
 #..............................................................
-apply(clst_ret, 1, sd)
-mastermap.results.out <- mastermap.results[, c(1, 352:ncol(mastermap.results))]
-write_csv(mastermap.results.out, path = "~/Desktop/inb_clst_results.csv")
+write_csv(mastermap.results, path = "~/Desktop/inb_clst_results.csv")
 
 #..............................................................
 # out
 #..............................................................
 write_rds(x = clst_ret, path = "data/derived_data/clst_inbreeding_dat/clst_inbreeding_runs.RDS")
 
+#..............................................................
+# play
+#..............................................................
+names(clst_ret.list) <- sapply(clst_ret.list, function(x){as.character(x$param_set)})
 
+outtest <- readRDS(filepaths[grepl("paramset_199.RDS", filepaths)])
+plot(outtest$m_run)
+outtest.firun <- do.call("rbind.data.frame",  outtest$fi_run)
+plot(outtest.firun[,3])
+
+bettest <- readRDS(filepaths[grepl("paramset_216.RDS", filepaths)])
+plot(bettest$m_run)
+bettest.firun <- do.call("rbind.data.frame",  bettest$fi_run)
+plot(bettest.firun[,3])
+
+
+plot(outtest.firun[, 134])
+plot(bettest.firun[, 134])
+max(outtest.firun[99e2:1e4, 134] - lag(outtest.firun[9899:1e3, 134])[2:102])
+max(outtest$m_run[99e2:1e4] - lag(outtest$m_run[9899:1e4])[2:102])
+
+max(bettest.firun[99e2:1e4, 134] - lag(bettest.firun[9899:1e4, 134])[2:102])
+max(bettest$m_run[99e2:1e4] - lag(bettest$m_run[9899:1e4])[2:102])
+
+plot(outtest$m_run)
+plot(bettest$m_run)
 
 
 # sanity
