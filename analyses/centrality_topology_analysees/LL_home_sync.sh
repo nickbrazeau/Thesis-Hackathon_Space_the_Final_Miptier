7b05587c298aa0be27c5aa66aac23521914cd597
Nicholas Brazeau <nbrazeau@med.unc.edu>
2020-07-20 23:40:57 -0400

    update net top

--- analyses/centrality_topology_analysees/LL_home_sync.sh
+++
@@ -1,1 +0,0 @@
-rsync -av /Users/nickbrazeau/Documents/GitHub/Space_the_Final_Miptier/data/derived_data/clst_inbreeding_dat/paramset/ nfb@longleaf.unc.edu:/proj/ideel/meshnick/users/NickB/Projects/Space_the_Final_Miptier/data/derived_data/clst_inbreeding_dat/paramset/
