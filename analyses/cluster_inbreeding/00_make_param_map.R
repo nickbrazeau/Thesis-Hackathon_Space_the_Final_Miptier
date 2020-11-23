5b878b1c449a95c80a38d43185873926ed66d50c
nickbrazeau <nbrazeau@med.unc.edu>
2020-11-23 00:08:06 -0500

    look at with monoclonals

--- analyses/cluster_inbreeding/00_make_param_map.R
+++ analyses/cluster_inbreeding/00_make_param_map.R
@@ -15,6 +15,15 @@ library(tidyverse)
 # read in GE as import
 mtdt <- readRDS("data/derived_data/sample_metadata.rds")
 clsts <- sort(unique(c(mtdt$hv001)))
+
+#......................
+# subset to monocloanl clusters
+#......................
+monoclonals <- readRDS("data/derived_data/RMCL_results/monoclonals.rds")
+clsts <- clsts[clsts %in% monoclonals$hv001]
+#......................
+# f and ms
+#......................
 fs <- seq(0.1, 0.9, by = 0.1)
 ms <- c(1e-12, 1e-10, 1e-8, 1e-6, 1e-5)
 
@@ -80,71 +89,3 @@ write.table(x = snakemap,
             file = "data/derived_data/clst_inbreeding_dat/paramset_clust/snake_map.txt",
             quote = F, sep = "\t", col.names = T, row.names = F)
 
-
-#....................................................................................................
-# SNAKEMAP for Voroni Province Tesselations
-#....................................................................................................
-#..............................................................
-# read in data
-#..............................................................
-# read voroni tesselation territories as import
-vrdf <- readRDS("data/distance_data/voroni_base.RDS")
-provs <- sort(unique(c(vrdf$IPUMSID)))
-fs <- seq(0.1, 0.9, by = 0.1)
-ms <- c(1e-12, 1e-10, 1e-8, 1e-6, 1e-5)
-
-
-# prov voroni tesselation start params
-fparams <- as.data.frame(matrix(fs, nrow = length(fs), ncol = length(provs)))
-colnames(fparams) <- provs
-fparams <- lapply(1:length(ms), function(x){return(fparams)}) %>%
-  do.call("rbind.data.frame", .)
-fparams <- fparams[order(fparams$`1`), ]
-# all f and m params
-prov_params <- cbind(fparams, "m" = ms)
-
-#..............................................................
-# Add in Learning Rate
-#..............................................................
-f_learningrate <- c(1e-7, 1e-6, 1e-5, 1e-4)
-m_learningrate <- c(1e-18, 1e-15, 1e-12, 1e-10)
-learningrates <- expand.grid(f_learningrate, m_learningrate)
-colnames(learningrates) <- c("f_learningrate", "m_learningrate")
-
-# prov voroni tesselation start params
-prov_lrandparams <- lapply(1:nrow(learningrates), function(x){return(prov_params)}) %>%
-  do.call("rbind.data.frame", .)
-prov_lrandparams <- prov_lrandparams[order(prov_lrandparams$`1`), ]
-prov_lrandparams <- cbind(prov_lrandparams, learningrates)
-
-#..............................................................
-# add in inputs
-#..............................................................
-lrandparams.migrate <- prov_lrandparams
-lrandparams.migrate$inputpath <- "data/derived_data/clst_inbreeding_dat/migrate_gens.RDS"
-lrandparams.migrate$full_matrix <- TRUE
-lrandparams <- lrandparams.migrate
-#..............................................................
-# split these and write them out
-#..............................................................
-lrandparams.list <- split(lrandparams, 1:nrow(lrandparams))
-paramsnames <- paste0("paramset_", 1:length(lrandparams.list), "_prov")
-
-write_out_params <- function(paramset, outname, outdir){
-  saveRDS(object = paramset, file = paste0(outdir, outname, ".RDS"))
-}
-
-outdir <- "data/derived_data/clst_inbreeding_dat/paramset_prov/"
-dir.create(outdir, recursive = T)
-mapply(write_out_params, paramset = lrandparams.list, outname = paramsnames, outdir = outdir)
-
-#..............................................................
-# make master map file
-#..............................................................
-snakemap <- data.frame(parampath = paramsnames)
-mastermap <- cbind.data.frame(lrandparams, snakemap)
-saveRDS(mastermap, "data/derived_data/clst_inbreeding_dat/paramset_prov/mastermap.RDS")
-colnames(snakemap) <- c("#parampath")
-write.table(x = snakemap,
-            file = "data/derived_data/clst_inbreeding_dat/paramset_prov/snake_map.txt",
-            quote = F, sep = "\t", col.names = T, row.names = F)
