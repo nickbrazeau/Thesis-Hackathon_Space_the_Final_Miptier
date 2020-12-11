b58c0a9a5b9f88ad1958e5230856b65263732826
nickbrazeau <nbrazeau@med.unc.edu>
2020-12-11 11:47:59 -0500

    update themes

--- R/themes.R
+++ R/themes.R
@@ -1,8 +1,7 @@
 library(ggplot2)
-map_theme <- theme(plot.title = element_text(family = "Helvetica", face = "bold", hjust = 0.5, size = 14),
-                   axis.title = element_text(family = "Helvetica", face = "bold", hjust = 0.5, size = 12),
-                   axis.text = element_text(family = "Helvetica", hjust = 0.5, size = 11),
-                   legend.position = "bottom",
+map_theme <- theme(axis.text = element_blank(),
+                   axis.title = element_blank(),
+                   legend.position = "right",
                    legend.title = element_text(family = "Helvetica", face = "bold", vjust = 0.85, size = 12),
                    legend.text = element_text(family = "Helvetica", hjust = 0.5, vjust = 0.5, angle = 90, size = 10),
                    panel.background = element_rect(fill = "transparent"),
