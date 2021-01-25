aa9ff8c7b7fc8762d35298e212961059b5a60078
nickbrazeau <nbrazeau@med.unc.edu>
2021-01-25 14:08:51 -0500

    get mincost and updates for drake

---
+++ analyses/clust_inbreeding/03-get_mincost.R
@@ -0,0 +1,49 @@
+## .................................................................................
+## Purpose: Get MinCost of DRC DISC fits
+##
+##
+## Notes:
+## .................................................................................
+library(tidyverse)
+
+#............................................................
+# read in results
+#...........................................................
+ret_read_wrapper <- function(path) {
+  readRDS(path) %>%
+    dplyr::mutate(datalvl = stringr::str_extract(inputpath, "allsmpls|coione"),
+                  distlvl = stringr::str_extract(inputpath, "gcdist|roaddist|airdist")) %>%
+    dplyr::select(-c("inputpath", "clst_names", "id"))
+}
+
+# get paths and read in results
+retmap <- tibble::tibble(paths = list.files("results/cluster_inbreed_ests/",
+                                            full.names = T)) %>%
+  dplyr::mutate(ret = purrr::map(paths, ret_read_wrapper)) %>%
+  tidyr::unnest(cols = "ret") %>%
+  dplyr::mutate(cost = purrr::map(discentret, "cost"),
+                mincost = purrr::map_dbl(cost, function(x){x[length(x)]})) %>%
+  dplyr::select(-c("paths"))
+
+#......................
+# look at cost distributions
+#......................
+retmap %>%
+  dplyr::select(c("datalvl", "distlvl", "mincost")) %>%
+  ggplot() +
+  geom_boxplot(aes(x = mincost)) +
+  coord_flip() +
+  facet_wrap(datalvl ~ distlvl, scales = "free")
+
+
+
+#............................................................
+# save out min costs
+#...........................................................
+out <- retmap %>%
+  dplyr::group_by(datalvl, distlvl) %>%
+  dplyr::filter(mincost == min(mincost))
+
+
+dir.create("results/cluster_inbreed_ests/min_cost_inbreedingresults/", recursive = T)
+saveRDS(out, "results/cluster_inbreed_ests/min_cost_inbreedingresults/min_cost_inbreedingresults.RDS")
