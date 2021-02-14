ad5aa0f89619dd2a1eb688db4c25930bc75c39da
nickbrazeau <nbrazeau@med.unc.edu>
2021-02-14 16:52:41 -0500

    iters to 1e4

--- analyses/clust_inbreeding/02-get_discent.R
+++ analyses/clust_inbreeding/02-get_discent.R
@@ -44,7 +44,7 @@ drake_wrapper <- function(batchset_df) {
                                            f_learningrate = f_learn,
                                            m_learningrate = m_learn,
                                            full_matrix = FALSE,
-                                           steps = 5e4,
+                                           steps = 1e4,
                                            report_progress = FALSE,
                                            return_verbose = FALSE)
     return(ret)
