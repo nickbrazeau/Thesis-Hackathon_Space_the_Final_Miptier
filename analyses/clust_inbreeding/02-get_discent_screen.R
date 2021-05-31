669cba5121b8b4900aba67ce51a4c2f489be95f5
nickbrazeau <nbrazeau@med.unc.edu>
2021-05-31 13:59:24 -0400

    better learning rates

--- analyses/clust_inbreeding/02-get_discent_screen.R
+++ analyses/clust_inbreeding/02-get_discent_screen.R
@@ -71,8 +71,9 @@ monoclonals_gengeodatpaths <- list.files("data/derived_data/coione_clst_inbreedi
 gengeodatpaths <- c(allsmpls_gengeodatpaths, monoclonals_gengeodatpaths)
 fs <- seq(0, 1, by = 0.1)
 ms <- c(1e-14, 1e-13, 1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3)
-learningrate <- c(1e-20, 1e-19, 1e-18, 1e-17, 1e-16, 1e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-10,
-                  1e-9, 1e-8, 1e-7, 1e-6, 1e-5)
+learn <- c(1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5,
+           1e-4, 1e-3, 1e-2, 0.05,
+           0.1, 0.5, 0.75, 1)
 param_map <- expand.grid(gengeodatpaths, fs, ms, learningrate) %>%
   tibble::as_tibble(., .name_repair = "minimal") %>%
   magrittr::set_colnames(c("inputpath", "f_start", "m_start", "learn"))
