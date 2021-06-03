676b0801629aa5bbb3c32d930e64f1607a91b859
nickbrazeau <nbrazeau@med.unc.edu>
2021-06-03 12:01:31 -0400

    discent update slurm

--- analyses/simwork/06-run_discent_screen.R
+++ analyses/simwork/06-run_discent_screen.R
@@ -6,7 +6,7 @@
 #............................................................
 #  setup
 #...........................................................
-workers <- 1028 # slurm array jobs to partition across
+workers <- 800 # slurm array jobs to partition across
 library(tidyverse)
 library(drake)
 
