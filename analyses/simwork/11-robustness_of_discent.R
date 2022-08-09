4137d7da62a5ac77eedc8961fc1656783c11380f
nickbrazeau <nbrazeau1@gmail.com>
2022-08-09 22:40:14 +0300

    tidy up

--- analyses/simwork/11-robustness_of_discent.R
+++
@@ -1,7 +0,0 @@
-## .................................................................................
-## Purpose: Error associated with Simulations
-##
-## Notes:
-## .................................................................................
-# do sum square of error for median F values versus optimize
-# do this for m parameter to
